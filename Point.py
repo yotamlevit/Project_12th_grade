87056503566229df59f5c2c696a03ee60b671d96
yotamlevit <43199124+yotamlevit@users.noreply.github.com>
2019-01-08 14:44:47 +0200

    Add files via upload

---
+++ Point.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def main():
+    """
+    Add Documentation here
+    """
+    pass  # Replace Pass with Your Code
+
+
+if __name__ == '__main__':
+    main()
---
+++ analytic_geometry.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+class StraightLine:
+
+    def __init__(self, point1, point2):
+        self.m = self.incline(point1, point2)
+        self.b =
+    def equation(self, point1, point2):
+        m = StraightLine.incline(point1, point2)
+        equ =
+
+    def incline(self, point1, point2):
+        return (point1.y - point2.y) / (point1.x - point2.x)
+
+def main():
+    """
+    Add Documentation here
+    """
+    pass  # Replace Pass with Your Code
+
+
+if __name__ == '__main__':
+    main()
